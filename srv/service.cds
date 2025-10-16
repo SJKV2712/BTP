bf93c49ef3668b5b6f6bc5b4fa43ed5fc4203454
Keerthi Vaasan SJ <keerthivaasan.sj@smartshinetech.com>
2025-10-16 07:32:17 +0000

    add & hide the URL

--- srv/service.cds
+++ srv/service.cds
@@ -1,3 +1,6 @@
+@protocol: 'rest'  // To Hide the URL to end
 service btpapp {
     function printstring (input: String) returns String;
+
+    function add (num1: Integer,num2: Integer) returns Integer;
 }
--- srv/service.js
+++ srv/service.js
@@ -2,8 +2,18 @@ const cds = require('@sap/cds')
 
 module.exports = cds.service.impl(async function(srv) {
 
-    srv.on('printstring', req=> {
-        console.log(req.data.input)  //print the requested data
-        return  `${req.data.input} World`
-    })
+    srv.on('printstring', req=> 
+         {
+            console.log(req.data.input)  //print the requested data
+            return  `${req.data.input} World`
+         })
+    srv.on('add',req=>
+          {
+            console.log('Number 1: %d',req.data.num1)   // To print number 1
+            console.log('Number 2: %d',req.data.num2)   // To print number 2
+            let result = req.data.num1 + req.data.num2;
+            console.log('Result by adding two numbers : %d', result)
+            return result
+          }
+    )
 })
